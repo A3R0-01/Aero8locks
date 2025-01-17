844ecaf094539104776d720f6404bf928fcf7026
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2025-01-17 16:31:25 +0200

    Keys Init

--- crypto/keys.go
+++ crypto/keys.go
@@ -1,7 +1,89 @@
 package crypto
 
-import "crypto/ed25519"
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+)
+
+const (
+	privKeyLen = 64
+	pubKeyLen  = 32
+	seedLen    = 32
+	addressLen = 20
+)
 
 type PrivateKey struct {
 	key ed25519.PrivateKey
+}
+
+func (p *PrivateKey) Bytes() []byte {
+	return p.key
+
+}
+func GeneratePrivateKey() *PrivateKey {
+	seed := make([]byte, seedLen)
+	_, err := io.ReadFull(rand.Reader, seed)
+	if err != nil {
+		panic(err)
+
+	}
+	return &PrivateKey{
+		key: ed25519.NewKeyFromSeed(seed),
+	}
+}
+
+func (p *PrivateKey) Sign(msg []byte) *Signature {
+	return &Signature{
+		value: ed25519.Sign(p.key, msg),
+	}
+}
+
+func (p *PrivateKey) Public() *PublicKey {
+	b := make([]byte, pubKeyLen)
+
+	copy(b, p.key[32:])
+	return &PublicKey{
+		key: b,
+	}
+
+}
+
+type PublicKey struct {
+	key ed25519.PublicKey
+}
+
+func (pb *PublicKey) Bytes() []byte {
+	return pb.key
+}
+
+func (pb *PublicKey) Address() Address {
+	return Address{
+		value: pb.key[len(pb.key)-addressLen:],
+	}
+}
+
+type Signature struct {
+	value []byte
+}
+
+func (s *Signature) Bytes() []byte {
+	return s.value
+}
+
+func (s *Signature) Verify(pubKey *PublicKey, msg []byte) bool {
+	return ed25519.Verify(pubKey.key, msg, s.Bytes())
+}
+
+type Address struct {
+	value []byte
+}
+
+func (a *Address) String() string {
+	return hex.EncodeToString(a.value)
+}
+
+func (a *Address) Bytes() []byte {
+	return a.value
 }
--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -1,1 +1,39 @@
 package crypto
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePrivateKey(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	assert.Equal(t, len(privKey.Bytes()), privKeyLen)
+
+	pubKey := privKey.Public()
+	assert.Equal(t, len(pubKey.Bytes()), pubKeyLen)
+}
+
+func TestPrivateKeySign(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	msg := []byte("foo bar baz")
+	sig := privKey.Sign(msg)
+
+	// test with a valid public key
+	assert.True(t, sig.Verify(pubKey, msg))
+
+	// test with an invalid public key
+	pkey := GeneratePrivateKey()
+	fakePubKey := pkey.Public()
+	assert.False(t, sig.Verify(fakePubKey, msg))
+}
+
+func TestPublicKeyToAddress(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	address := pubKey.Address()
+	assert.Equal(t, addressLen, len(address.Bytes()))
+	fmt.Println(address.String())
+}
